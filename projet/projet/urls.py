79a7770753673e477884b2f420dd8a14f8c0972d
MFB <158731846+fahdBENS@users.noreply.github.com>
2024-08-04 13:14:28 +0100

    Delete projet directory

--- projet/projet/urls.py
+++
@@ -1,9 +0,0 @@
-# projet/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('suivi.urls')),  # Inclure les URLs de l'application 'suivi'
-]
